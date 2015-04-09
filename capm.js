b2edc8fc513e11e4598ee124ef74f6a3a3f0fe9d
Chris Makler <chris.makler@gmail.com>
2015-04-09 13:29:48 -0700

    working graphically
    thanks to karthik for the optimization tips!

--- capm.js
+++ capm.js
@@ -5,28 +5,48 @@ app.controller('Controller', function ($scope) {
     $scope.params = {
         beta: 1,
         rf: 0.01,
-        T: 2,
+        T: 7,
         sigma: 0.17,  // historic volatility of US market
         MRP: 0.06,
         S0: 1,
-        numDraws: 5
+        numDraws: 10
+    };
+
+    function debounce(fn,time) {
+        var timerID;
+        return function() {
+            var self = this;
+            var args = arguments;
+            if(timerID) {
+                clearTimeout(timerID)
+            }
+            timerID = setTimeout(function(){
+                fn.apply(self,args)
+            },time)
+        }
     }
 
     // create a 121 x numDraws matrix of shocks
     function updateShocks() {
-        $scope.epsilon = [];
+
+        var numDraws = 1000; // can set to $scope.params.numDraws
+        var T = 50; // can set to $scope.params.T
+
+        $scope.epsilon = new Array(numDraws);
 
         // for each stock
-        for(var s=0; s<$scope.params.numDraws; s++) {
+        for(var s=0; s<numDraws; s++) {
 
             // initialize with shock of zero in period 0
-            stock_shocks = [0];
+            var stock_shocks = new Array(T*12 + 1);
+
+            stock_shocks[0] = 0;
 
             // generate an array of 120 shocks (values of epsilon) ~ N(0,1)
-            for(var t=1; t<=12*$scope.params.T; t++) {
-                stock_shocks.push(d3.random.normal(0,1)())
+            for(var t=1; t<=T*12; t++) {
+                stock_shocks[t] = d3.random.normal(0,1)();
             }
-            $scope.epsilon.push(stock_shocks);
+            $scope.epsilon[s] = stock_shocks;
         }
     }
 
@@ -35,11 +55,13 @@ app.controller('Controller', function ($scope) {
     function stockData(params, epsilon_array) {
         
         // initialize with a stock value of S0 in period 0
-        var stock_values = [params.S0],
-            sum = 0;
+        var stock_values = new Array(12*params.T + 1), sum = 0;
+
+        stock_values[0] = params.S0;
+
         for(var t=1; t<=12*params.T; t++) {
             sum += epsilon_array[t];
-            stock_values.push(params.S0*Math.exp((params.t_coeff * t) + (params.sum_coeff * sum )));
+            stock_values[t] = params.S0*Math.exp((params.t_coeff * t) + (params.sum_coeff * sum ));
         }
         return stock_values;
     }
@@ -57,28 +79,33 @@ app.controller('Controller', function ($scope) {
             T: params.T
         };
         
-        var data_matrix = []
+        var data_matrix = new Array(params.numDraws);
         for(var s=0; s<params.numDraws; s++) {
-            data_matrix.push(stockData(stockValueParams, shocks[s]));
-        };
+            data_matrix[s] = stockData(stockValueParams, shocks[s]);
+        }
         return data_matrix;
     }
 
     function valuesInPeriod(data_matrix,t) {
-        return data_matrix.map(function(stock_values){return stock_values[t]}).sort('ascending');
+        return data_matrix.map(function(stock_values){return stock_values[t]});
     }
 
-    function medianInPeriod(data_matrix,t) {
-        return d3.median(valuesInPeriod(data_matrix,t));
-    }
-
-    $scope.median = function() {
-        var median_array = [];
+    $scope.plotdata = function(data_matrix) {
+        var data_array = [],
+            vt = [];
         for(var t=0; t<=12*$scope.params.T; t++) {
-            median_array.push(medianInPeriod($scope.data,t));
+            vt = valuesInPeriod(data_matrix,t).sort(d3.ascending);
+            data_array[t] = {
+                date: t,
+                pct05: d3.quantile(vt,0.05),
+                pct25: d3.quantile(vt,0.25),
+                pct50: d3.quantile(vt,0.50),
+                pct75: d3.quantile(vt,0.75),
+                pct95: d3.quantile(vt,0.95)
+            };
         }
-        return median_array;
-    }
+        return data_array;
+    };
 
     function update() {
         $scope.params.beta = parseFloat($scope.params.beta);
@@ -86,11 +113,14 @@ app.controller('Controller', function ($scope) {
         $scope.params.T = parseInt($scope.params.T);
         $scope.params.sigma = parseFloat($scope.params.sigma);
         $scope.params.MRP = parseFloat($scope.params.MRP);
-        updateShocks();
+        var d = new Date().getTime();
         $scope.data = generateData($scope.params, $scope.epsilon);
+        console.log("generateData time ", (new Date().getTime() - d)/1000, "s")
+        makeChart($scope.plotdata($scope.data));
+
     }
-    
-    $scope.$watchCollection('params', update);
+    //$scope.data = generateData($scope.params, $scope.epsilon);
+    $scope.$watchCollection('params', debounce(update,100));
 
     update();
     
